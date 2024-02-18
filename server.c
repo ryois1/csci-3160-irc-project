87529d0b498cab85e680fd1830e3ee4edfd349e5
hidden1nin <andrewletterman10@gmail.com>
2024-02-18 18:11:46 -0500

    Semi-Wipe, kept some stuff while reorganizing the project and reducing complexity greatly. Added interfaces for both servers and sockets.

--- server.c
+++ server.c
@@ -12,19 +12,19 @@
 
 int main() {
     IServerSocket webserver = WebServerInstance.base;
-    IServerSocket cliserver = CLIServerInstance.base;
+    IServerSocket cliserver = cliServerInstance.base;
 
     //TODO
     //Add variable called messages that both servers have access too, it should be atomic (think our most recent lab)
     //Add a list of ISocket, that both have access to, then when one recieves a message it can simply go through the list and forward the message to the other sockets.
 
     cliserver.initialize();
-    cliserver.bind();
+    cliserver.bind("localhost",8888);
     cliserver.cleanup();
 
 
     webserver.initialize();
-    webserver.bind();
+    webserver.bind("localhost",80);
     webserver.cleanup();
 
     return 0;
