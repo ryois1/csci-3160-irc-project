44ce08f6d8f1ce978e97391ffb1b9cd1d0a39eda
hidden1nin <andrewletterman10@gmail.com>
2024-02-18 20:54:11 -0500

    Tried to splice ping pong example into CLI socket

--- sockets/CLISocket.c
+++ sockets/CLISocket.c
@@ -1,4 +1,11 @@
-// Socket.c
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "ISocket.c"
 
@@ -7,12 +14,52 @@ typedef struct {
     ISocket base;
     // Add any specific members for the Socket
     // ...
+    int sfd;
 } CLISocket;
 
 // Implementation of ISocket methods for Socket
-static int Socket_open(void) {
+int Socket_open(void) {
     // Implementation for opening a socket
     printf("Opening client socket cli.\n");
+    // Variables
+    int              bytes;
+    char             buf[BUF_SIZE];
+    ssize_t          nread;
+    struct addrinfo  hints;
+    struct addrinfo  *result;
+
+    /* Obtain address matching host/port. */
+
+    // Initialize address hints structure
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* TCP socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_TCP; /* TCP */
+
+    // Get the address from the variables
+    if (getaddrinfo("localhost",8888, &hints, &result) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a socket
+    this.sfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (this.sfd == -1) {
+        fprintf(stderr, "socket: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Free the address information since it's no longer needed
+    freeaddrinfo(result);
+
+    // Successfully connected to the server
+    printf("Client writing ping\n");
+    bytes = write(this.sfd, "ping", 5); // Send "ping" to the server
+    bytes = read(this.sfd, buf, 5);       // Read the response from the server
+    printf("PID: %d; client received %s\n", getpid(), buf);
+
+
     return 0;
 }
 
