1ac69385d64a05aea172f5cdf02d4f0a7ea01d66
hidden1nin <andrewletterman10@gmail.com>
2024-02-18 21:25:54 -0500

    Tried to splice ping pong example into CLI socket

--- servers/CLIServer.c
+++ servers/CLIServer.c
@@ -1,7 +1,12 @@
 // CLIServer.c
-
+#include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // Define the CLIServer class
 typedef struct {
@@ -14,6 +19,92 @@ typedef struct {
 static void initializeCLIServer(void) {
     // Initialization logic specific to CLIServer
     printf("\x1B[32m   Initializing CLI Server... \n\033[0m");
+
+    //Declare variables
+    int						 reuse_port = 1;
+	int						 connection;
+	int                      sfd, s;
+	char                     buf[32];
+	char					 portnum = "8888";
+	ssize_t                  nread;
+	socklen_t                peer_addrlen;
+	struct addrinfo          hints;
+	struct addrinfo          *result, *rp;
+	struct sockaddr_storage  peer_addr;
+
+
+
+    //Declare even more variables
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM;/* TCP protocol socket */
+	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+	hints.ai_protocol = IPPROTO_TCP;/* TCP */
+	hints.ai_canonname = NULL;
+	hints.ai_addr = NULL;
+	hints.ai_next = NULL;
+
+
+
+    //Get ADDR and whatnot
+    int s = getaddrinfo(NULL, portnum, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create socket and set SO_REUSEPORT option */
+    sfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (sfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port)) < 0) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sfd, result->ai_addr, result->ai_addrlen) != 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    
+
+
+    freeaddrinfo(result);           /* No longer needed */
+
+
+
+	if (rp == NULL) {               /* No address succeeded */
+		fprintf(stderr, "Could not bind\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Listen to the gorram socket */
+	if (listen(sfd, 1) < 0) {
+		fprintf(stderr, "Error in listen: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Calling accept\n");
+	connection = accept(sfd, NULL, NULL);
+	if (connection < 0) {
+		fprintf(stderr, "Error in accept: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	/* Do the ping-pong thing */
+	read(connection, buf, 5);
+	printf("PID: %d; server received %s\n", getpid(), buf);
+	strcpy(buf, "pong");
+	printf("Server writes %s\n", buf);
+	write(connection, buf, 5);
+
+	close(connection);			/* Tear down the session with client */
+
+	return 0;
 
 }
 
