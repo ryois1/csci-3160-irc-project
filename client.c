1e59008bec61c5344180e9e7340cdcbfa9defd8a
hidden1nin <andrewletterman10@gmail.com>
2024-02-18 19:03:01 -0500

    Added default cli

--- client.c
+++ client.c
@@ -10,10 +10,21 @@
 
 int main(int argumentCount, char *arguments[])
 {
-    if (argumentCount != 4)
+    if(argumentCount>1&&argumentCount<5){
+        fprintf(stderr, "\x1B[31mNot Enough arguments, using default connection.\n\033[0m");
+        //Change the argument count and assign it to a new array;
+        argumentCount = 5;
+        arguments = (char **)malloc(sizeof(char *) * 5);
+        arguments[1] = "localhost";
+        arguments[2] = "8888";
+        arguments[3] = "guest";
+        arguments[4] = "password";
+
+    }
+    if (argumentCount != 5)
     {
         //Prints as a red error string.
-        fprintf(stderr, "\x1B[31mUsage: %s <server_address> <server_port> <client_username>\n\033[0m", arguments[0]);
+        fprintf(stderr, "\x1B[31mUsage: %s <server_address> <server_port> <client_username> <client_password>\n\033[0m", arguments[0]);
         exit(EXIT_FAILURE);
     }
     return 0;
