888d751173b536a7b7d9b39b9f9168e1dc831599
hidden1nin <andrewletterman10@gmail.com>
2024-03-06 15:32:48 -0500

    FIXED THIS

--- client.c
+++ client.c
@@ -44,12 +44,6 @@ int main(int argumentCount, char *arguments[])
     ISocket socket = cliSocketInstance.base;
     socket.open(&socket);
 
-    
-    socket.send(&socket,"BEFORE FORK",32);
-    socket.send(&socket,"BEFORE FORK",32);
-    socket.send(&socket,"BEFORE FORK",32);
-    socket.send(&socket,"BEFORE FORK",32);
-
     //Fork here, call recieve
     //create child, send child through socket.receive
     pid_t parent_pid = getpid();
