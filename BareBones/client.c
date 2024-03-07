4cc7042cb27fb2adfbaef8eda4a6add356410934
hidden1nin <andrewletterman10@gmail.com>
2024-03-07 14:00:35 -0500

    barebones

---
+++ BareBones/client.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+
+int main() {
+    //Clears the screen
+    printf("\e[1;1H\e[2J");
+
+    int file =connect();
+    while(1){
+        recieve(file);
+    }
+
+}
+
+
+
+int connect(){
+    int sfd;
+
+    printf("Opening client socket cli.\n");
+    // Variables
+    int              bytes;
+    char             buf[32];
+    ssize_t          nread;
+    struct addrinfo  hints;
+    struct addrinfo  *result;
+
+    /* Obtain address matching host/port. */
+
+    // Initialize address hints structure
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* TCP socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_TCP; /* TCP */
+
+    // Get the address from the variables
+    if (getaddrinfo("127.0.0.1","9000", &hints, &result) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a socket
+    clisocket->sfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    int connectresult = connect(clisocket->sfd, result->ai_addr, result->ai_addrlen);
+    if (connectresult == -1) {
+        fprintf(stderr, "socket: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Free the address information since it's no longer needed
+    freeaddrinfo(result);
+
+    // Successfully connected to the server
+    //TODO determine how to fork properly so that the original program can continue or even ask the user for inputs while also allowing the socket to receive messages.
+    printf("Successfully connected!!\n");
+    return sfd;
+}
+
+void recieve(int file){
+    char bufRec[32];
+    bufRec[0]='\0';
+    fcntl(file, F_SETFL, SOCK_NONBLOCK); //non blocking read
+    int reading = read(file, bufRec, 32);
+    if(reading<=0)return;
+    printf("received %s\n", bufRec);
+}
---
+++ BareBones/server.c
@@ -0,0 +1,94 @@
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <sys/select.h> 
+#include <stdatomic.h>
+#include <sys/mman.h>
+
+int connection_count = 0;
+int connections[32];
+
+int main() {
+    int server = start();
+
+    while(1){
+        connect(server);
+    }
+}
+
+int start(){
+    // Initialization logic specific to CLIServer
+    printf("\x1B[32m   Initializing CLI Server... \n\033[0m");
+
+    //Declare variables
+    int sfd;
+    char buf[32];
+    struct addrinfo hints, *result, *rp;
+    const char *portnum = "9000";
+    int reuse_port = 1;
+
+    
+
+    //Declare more variables
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* TCP socket */
+    hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
+    hints.ai_protocol = IPPROTO_TCP;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    //Get the address
+    int s = getaddrinfo(NULL, portnum, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create socket and set SO_REUSEPORT option */
+    sfd = socket(result->ai_family, result->ai_socktype | SOCK_NONBLOCK, result->ai_protocol);
+    if (sfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port)) < 0) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sfd, result->ai_addr, result->ai_addrlen) != 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    /* No longer needed */
+    freeaddrinfo(result);           
+
+
+	/* Listen to the gorram socket */
+	if (listen(sfd, 1) < 0) {
+		fprintf(stderr, "Error in listen: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+    return sfd
+}
+
+void connect(int file){
+    int connection = accept(sfd, NULL, NULL);
+
+    if (connection >0) {
+        printf("Accepted %d\n", connection);
+        connections[connection_count] = connection;
+        connection_count = connection_count + 1;
+
+        printf("Connected count amount: %d\n", connection_count);
+    }
+}
