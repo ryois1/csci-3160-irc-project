30f98c65ae2547645873dfb6c78d2fc90073c8cc
hidden1nin <andrewletterman10@gmail.com>
2024-03-07 16:02:31 -0500

    barebones

--- BareBones/bclient.c
+++ BareBones/bclient.c
@@ -84,7 +84,8 @@ void checkrecieve(int file){
 }
 
 void sendhello(int file){
-    char words[32] = concatenate("Hello from: ",toString(getpid()));
+    char words[32] = "Hello From: ";
+    concatenate(words,toString(getpid()));
     int byteswritten = write(file,words , 32);
     // if(byteswritten < 0){
     //     printf("Error writing bytes.");
