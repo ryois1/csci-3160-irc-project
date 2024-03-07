3bcbe845224defaf11c536c04bf4877bda3ac9da
hidden1nin <andrewletterman10@gmail.com>
2024-03-06 20:05:08 -0500

    FIXED THIS

--- servers/CLIServer.c
+++ servers/CLIServer.c
@@ -126,7 +126,6 @@ static void initializeCLIServer() {
             char bufRec[32];
             fcntl(connections[i], F_SETFL, SOCK_NONBLOCK); //non blocking read
             read(connections[i], bufRec, 32);
-            fflush(connections[i]);
 
             //  printf("PID: %d; server received %s\n", getpid(), bufRec);
             //  read(connection, buf, 5);
